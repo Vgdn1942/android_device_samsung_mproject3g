388cf8563a575e7c8c5361b28cc1e1b8a4f4f3cd
Your Name <you@example.com>
2015-02-23 21:55:10 -0600

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,69 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
+
+-include device/samsung/smdk4412-common/BoardCommonConfig.mk
+
+# RIL
+BOARD_PROVIDES_LIBRIL := true
+BOARD_MODEM_TYPE := xmm6262
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/mproject3g/include
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/mproject3g/bluetooth
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
+TARGET_KERNEL_CONFIG := cyanogenmod_mproject3g_defconfig
+BOARD_KERNEL_CMDLINE := console=ttySAC2,115200 androidboot.selinux=permissive
+
+# ril
+BOARD_RIL_CLASS := ../../../device/samsung/mproject3g/ril/telephony/java
+
+# Selinux
+#BOARD_SEPOLICY_DIRS := \
+    device/samsung/mproject3g/selinux
+
+#BOARD_SEPOLICY_UNION := \
+    device.te \
+    domain.te \
+    file.te \
+    file_contexts \
+    init.te \
+    mediaserver.te \
+    rild.te \
+    system.te \
+    ueventd.te \
+    vold.te \
+    wpa_supplicant.te
+
+# assert
+TARGET_OTA_ASSERT_DEVICE := mproject3g,mproject3gxx,SM-C101
+
+# inherit from the proprietary version
+-include vendor/samsung/mproject3g/BoardConfigVendor.mk
+
+# Blobs
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/samsung/mproject3g/rootdir/fstab.smdk4x12
+RECOVERY_FSTAB_VERSION := 2
+
+# Camera wrapper
+TARGET_PROVIDES_CAMERA_HAL := true
---
+++ cm.mk
@@ -0,0 +1,26 @@
+# Release name
+PRODUCT_RELEASE_NAME := mproject3g
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/mproject3g/full_mproject3g.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := mproject3g
+PRODUCT_NAME := cm_mproject3g
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-C101
+PRODUCT_MANUFACTURER := samsung
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=mproject3gxx \
+    TARGET_DEVICE=mproject3g \
+    PRIVATE_BUILD_DESC="mproject3gxx-user 4.4.2 KOT49H C101XXUBNF4 release-keys" \
+    BUILD_FINGERPRINT="ro.build.fingerprint=samsung/mproject3gxx/mproject3g:4.4.2/KOT49H/C101XXUBNF4:user/release-keys"
